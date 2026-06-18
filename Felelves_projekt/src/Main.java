b92b2d92a5cd302c6ebd2509915eec893deee7a5
kistapir1 <kistapir0@gmail.com>
2026-06-18 02:39:30 +0200

    elv minden done (esetleg súgóban neved átírni hátha elkúrtam)

--- Felelves_projekt/src/Main.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.SwingUtilities;
-
-void main() {
-    SwingUtilities.invokeLater(() -> {
-        window myWindow = new window();
-        myWindow.setVisible(true); // Ez teszi láthatóvá az ablakot
-    });
-}
